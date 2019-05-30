c747f21221798a348d3eb1a6e87779ab3f023a09
QuangTuan <wayofthjn@gmail.com>
2019-05-30 15:23:01 +0700

    select data

---
+++ back-end/db.py
@@ -0,0 +1,43 @@
+﻿import pymysql.cursors  
+ 
+# Kết nối vào database.
+connection= None
+def connectDB():
+	global connection
+	connection = pymysql.connect(host='localhost',
+                            user='root',
+                            password='123456',                             
+                            db='cnpm',
+                            charset='utf8mb4',
+                            cursorclass=pymysql.cursors.DictCursor)
+	
+	
+             
+def getData():
+	sql = "SELECT * FROM `user`"
+	cursor = connection.cursor()
+	# Thực thi câu lệnh truy vấn (Execute Query).
+	cursor.execute(sql)
+	#return data
+	return cursor
+def getUser(id):
+	sql = "SELECT * FROM `user` WHERE user.id = {0}".format(id) 
+	cursor = connection.cursor()
+	# Thực thi câu lệnh truy vấn (Execute Query).
+	cursor.execute(sql)
+	#return data
+	return cursor
+def insertData(_id,mat_khau,tai_khoan):
+	sql ="INSERT into `user`(id, mat_khau, tai_khoan) values ({0},{1},{2})".format(_id,mat_khau,tai_khoan)
+	print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+	cursor = connection.cursor()
+	cursor.execute(sql)
+	connection.commit()
+def closeDB():
+	connection.close()
+#if __name__ == "__main__":
+#	connection = connectDB()
+#	results = getUser(1);
+#	for data in results:
+#		print(data)
+#	connectDB();
--- back-end/main.py
+++ back-end/main.py
@@ -1,12 +1,34 @@
 from flask import Flask,request,render_template
-
+import db
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
 	return render_template('layout.loitd',var1="ahihi")
 
-@app.route("/user/<username>")
-def findName(username):
-	print ("The method: {0}".format(request.method))
-	return "Hi {0}".format(username)
+@app.route("/users")
+def findName():
+	db.connectDB()
+	results = db.getData();
+	temp =[]
+	for data in results:
+		temp.append(data)
+	db.closeDB()
+	return json.dumps(temp)
+@app.route("/user/<int:username>")
+def findNameID(username):
+	db.connectDB()
+	results = db.getUser(username);
+	temp =[]
+	for data in results:
+		temp.append(data)
+	db.closeDB()
+	return json.dumps(temp)
+
+@app.route("/insert")
+def insertDB():
+	db.connectDB()
+	db.insertData("5","123456","huan")
+	db.closeDB()
+	return 200;
